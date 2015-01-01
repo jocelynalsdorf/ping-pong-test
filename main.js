3e1aee81ca2d4fc42d32d3c16480771a173a8436
jocelynalsdorf <jfrodsla@gmail.com>
2015-01-01 12:28:55 +0800

    loop works correctly, need to style and finish it up

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 $(document).ready(function() {
+	
+
+var userNum = prompt("Enter a whole number, such as 10");
+
 	var ping = "ping";
 	var pong = "pong";
 	var pp = "ping-pong";
+
+for (var i = 1; i <= userNum; i++) {
+
+	if (i % 15 === 0) {
+		$(".number-list").append(pp);
+		
+	}
+		else if(i % 3 === 0) {
+		$(".number-list").append(ping);	
+	}
+	else if (i % 5 === 0) {
+		$(".number-list").append(pong);	
+	}
+	 
+	else {	
+		$(".number-list").append(i);
+	}
+};
+
+
+
+
+
 
 });
